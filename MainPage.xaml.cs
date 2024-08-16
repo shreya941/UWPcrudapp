04c0021d751d0de55c9ce2538a70f01e4bd76d5c
Shreya dixit <116295874+shreya941@users.noreply.github.com>
2024-08-16 10:30:38 +0530

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Newtonsoft.Json;
+using MyFirstUI;
+using System.Collections.Generic;
+
+namespace MyFirstUI
+{
+    public sealed partial class MainPage : Page
+    {
+        private readonly HttpClient _httpClient;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _httpClient = new HttpClient();
+        }
+
+        private async void Save_Click(object sender, RoutedEventArgs e)
+        {
+            var employee = new Employee
+            {
+                FirstName = FirstName.Text,
+                LastName = LastName.Text,
+                Department = Department.Text,
+                Email = Email.Text,
+                Address = Address.Text,
+                PhoneNumber = PhoneNumber.Text,
+                DateOfBirth = DateTime.Parse(DateOfBirth.Text)
+            };
+
+            var json = JsonConvert.SerializeObject(employee);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            await _httpClient.PostAsync("http://localhost:5219/api/Employees", content);
+        }
+
+        private void ViewEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(ViewEmployeePage));
+        }
+    }
+}
---
+++ ViewEmployeePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Windows.UI.Xaml.Controls;
+using MyFirstUI;
+
+namespace MyFirstUI
+{
+    public sealed partial class ViewEmployeePage : Page
+    {
+        private readonly HttpClient _httpClient;
+
+        public ViewEmployeePage()
+        {
+            this.InitializeComponent();
+            _httpClient = new HttpClient();
+            LoadEmployees();
+        }
+
+        private async void LoadEmployees()
+        {
+            var response = await _httpClient.GetStringAsync("http://localhost:5219/api/Employees");
+            var employees = JsonConvert.DeserializeObject<List<Employee>>(response);
+            EmployeeListView.ItemsSource = employees;
+        }
+
+    }
+}
